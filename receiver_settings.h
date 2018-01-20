2023145c2b17d6e9034ac41d80ce6730c8310e1e
sdrabb <albe.merciai>
2018-01-20 16:28:29 +0100

    First commit

---
+++ receiver_settings.h
@@ -0,0 +1,35 @@
+#ifndef RECEIVER_SETTINGS_H
+#define RECEIVER_SETTINGS_H
+
+#include <QDialog>
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+namespace Ui {
+class Receiver_settings;
+}
+
+class Receiver_settings : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Receiver_settings(QWidget *parent = 0);
+    ~Receiver_settings();
+
+    void populateDataItem();
+
+public slots:
+    void add_new_receiver();
+    void save_receiver();
+
+
+private:
+    Ui::Receiver_settings *ui;
+    QSqlTableModel *model;
+    QSqlDatabase db;
+
+    bool initDatabase();
+    bool closeDatabase();
+};
+
+#endif // RECEIVER_SETTINGS_H
